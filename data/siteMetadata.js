202753ac0543f09b22d4241b369f1c9ccc1a628d
lesimoes <lesimoes0@gmail.com>
2023-08-19 18:44:01 -0300

    primeiro

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,23 +1,19 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  title: 'lesimoes blog',
+  author: 'Leandro Simões',
+  headerTitle: 'lesimoes',
+  description: 'Algumas coisas fazem sentido, outras não...',
+  language: 'pt-br',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  locale: 'en-US',
+  github: 'https://github.com/lesimoes',
+  youtube: 'https://www.youtube.com/channel/UCvdzKt-BgAePAxGQTLz_xjQ',
+  linkedin: 'https://www.linkedin.com/in/lesimoess/',
+  locale: 'pt-BR',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
